62937c79f0600dae6ce8aa2545e9364fc60371fc
Adil <mianadil850@gmail.com>
2021-01-21 23:20:43 +0500

    E-Clinics

---
+++ app/src/main/java/com/example/e_clinic/doctorMainScreen.kt
@@ -0,0 +1,11 @@
+package com.example.e_clinic
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class doctorMainScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_doctor_main_screen)
+    }
+}
