fd2261fefc58d59c4c807fae96a294b3d713be9a
Adil <mianadil850@gmail.com>
2021-01-21 22:34:16 +0500

    E-Clinics

---
+++ app/src/main/java/com/example/e_clinic/patientMainScreen.kt
@@ -0,0 +1,11 @@
+package com.example.e_clinic
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class patientMainScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_patient_main_screen)
+    }
+}
---
+++ app/src/main/java/com/example/e_clinic/patientProfile.kt
@@ -0,0 +1,11 @@
+package com.example.e_clinic
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class patientProfile : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_patient_profile)
+    }
+}
